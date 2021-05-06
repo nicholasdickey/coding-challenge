218e77f77a922a1ba7cc384e875ffbf9fbfc5df2
Nicholas Dickey <nicholas.dickey@hudsonwilde.com>
2021-05-06 12:07:30 +0000

    Add remaining cards display

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from 'Card'
-
+import { ReactComponent as Winner } from 'assets/winner.svg'
 import shuffle from 'deck'
 
 // eslint-disable-next-line
@@ -11,7 +11,7 @@ const ONE_HAND = 5
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
-const initGame = (comment: string) => {
+const initGame = comment => {
   // eslint-disable-next-line
   console.info('initGame', comment)
   return {
@@ -20,6 +20,7 @@ const initGame = (comment: string) => {
     deck: shuffle(),
     board: [] as CardDatum[],
     ended: false,
+    inflight: false,
   }
 }
 const deal = ({ cardsLeft, cardsUsed, deck }: Game, board: CardDatum[]) => {
@@ -58,7 +59,7 @@ function App() {
   const doDeal = async () => {
     let gameUpdate = game
     let board: CardDatum[] = []
-    setGame({ ...game, ...{ board } })
+    setGame({ ...game, ...{ board, inflight: true } })
     await sleep(50)
     console.info('doDeal')
     for (let pass = 1; pass <= ONE_HAND; pass += 1) {
@@ -69,8 +70,11 @@ function App() {
       // eslint-disable-next-line
       await sleep(150)
       if (pass === ONE_HAND && board.length <= 2) {
-        setGame({ ...game, ended: true })
+        setGame({ ...gameUpdate, ended: true, inflight: false })
         console.info('setEnded')
+      } else if (pass === ONE_HAND) {
+        // eslint-disable-next-line
+        setTimeout(() => setGame({ ...gameUpdate, ...{ inflight: false } }), 500)
       }
     }
   }
@@ -87,16 +91,31 @@ function App() {
     <div className="h-screen flex flex-col bg-gray-300 pb-0">
       <div
         className="w-auto relative h-full bg-gradient-to-b from-card-light via-card-medium to-card-dark"
-        style={{ minHeight: 240 }}
+        style={{ minHeight: 420 }}
       >
         {game.board.map((card, i) => {
-          return <Card key={`${i + 1}:key`} x={0} y={0} position={i} value={card} />
+          return (
+            <Card key={`${i + 1}:key`} x={0} y={0} position={i} value={card} ended={game.ended} />
+          )
         })}
+      </div>
+      <div className="absolute top-0 flex items-start justify-start w-full ">
+        <div className=" mx-auto  mt-1 h-full xl:h-1/2 w-1/4 lg:w-1/5 bg-black border rounded border-yellow-400 text-white font-rock text-xl sm:text-2xl text-center max-h-20 xl:max-h-30 ">
+          <div className="my-2">{game.cardsLeft}</div>
+          <div className="my-4">Cards Left</div>
+        </div>
+        {winner ? (
+          <div className="absolute top-18 flex items-start justify-start w-full h-40">
+            <div className=" mx-auto ">
+              <Winner />
+            </div>
+          </div>
+        ) : null}
       </div>
       <div className="absolute bottom-0 w-full h-1/2">
         <div
           className="relative h-full flex items-center justify-center z-10"
-          style={{ minHeight: 200 }}
+          style={{ minHeight: 300 }}
         >
           <div className="m-auto w-full flex items-center justify-center z-20">
             {game.ended ? (
@@ -118,18 +137,21 @@ function App() {
               <button
                 type="button"
                 onClick={doDeal}
+                disabled={game.inflight}
                 className={`"m-auto w-1/8 p-6 h-18  bg-yellow-400 opacity-80  hover:bg-blue-800 py-1 px-2 border border-black rounded"`}
               >
-                <div className="font-rock font-black text-4xl">DEAL</div>
+                <div className="font-rock font-black text-4xl">
+                  {game.inflight ? 'Dealing' : 'DEAL'}
+                </div>
               </button>
             )}
           </div>
-          <div className="absolute bottom-8 w-full h-2/7">
+          <div className="absolute bottom-28 w-full ">
             <div className=" h-full flex items-end justify-end mb-0 pb-0 mr-12">
               <button
                 type="button"
                 onClick={reset}
-                className="w-1/8 mb-8 py-1  opacity-80 hover:bg-blue-800  px-2 border border-yellow-400 rounded "
+                className="w-1/8 h-20 mb-2 py-1  opacity-80 hover:bg-blue-800  px-2 border border-yellow-400 rounded "
               >
                 <div className="font-rock text-yellow-400 text-lg">Reset</div>
               </button>
