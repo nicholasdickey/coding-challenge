7df510ff944e38e7e81f7250f0a6c163120a2d4e
Nicholas Dickey <nicholas.dickey@hudsonwilde.com>
2021-05-12 13:20:35 +0000

    Add docker service db to backend-node wokflow
    - create database as part of deps-and-codegen setup
    - create env var for GitHub workflows
    - connect either to db or localhost depending on environment

---
+++ node-server/create_db.js
@@ -0,0 +1,15 @@
+var pgtools = require('pgtools')
+
+const config = {
+  user: 'postgres',
+  host: process.env.GITHUB === '1' ? 'localhost' : 'db',
+  port: 5432,
+}
+// eslint-disable-next-line
+pgtools.createdb(config, 'uplifty', function(err, res) {
+  if (err) {
+    console.error(err)
+    process.exit(-1)
+  }
+  console.info(res)
+})
